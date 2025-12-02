337c111227eb42ce4272104b877dc401c59e55cd
Barti <bartiszabolcs24@gmail.com>
2025-12-02 21:45:16 +0100

    Add project files.

---
+++ Higher/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Higher;
+
+List<PlayerScore> scores = new List<PlayerScore>();
+
+scores.Add(new PlayerScore("Bela", 500, "Tetris", DateTime.Now));
+scores.Add(new PlayerScore("Anna", 750, "Pacman", DateTime.Now));
+scores.Add(new PlayerScore("John", 300, "Tetris", DateTime.Now));
+scores.Add(new PlayerScore("Zoe", 900, "Pacman", DateTime.Now));
+scores.Add(new PlayerScore("Mike", 450, "Tetris", DateTime.Now));
+scores.Add(new PlayerScore("Eva", 800, "Pacman", DateTime.Now));
+scores.Add(new PlayerScore("Tom", 600, "Tetris", DateTime.Now));
+scores.Add(new PlayerScore("Lily", 950, "Pacman", DateTime.Now));
+scores.Add(new PlayerScore("Sam", 400, "Tetris", DateTime.Now));
+scores.Add(new PlayerScore("Nina", 850, "Pacman", DateTime.Now));
+scores.Add(new PlayerScore("Rob", 700, "Tetris", DateTime.Now));
+scores.Add(new PlayerScore("Kate", 1000, "Pacman", DateTime.Now));
